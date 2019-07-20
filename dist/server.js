ce1575144a0691ffb7f5cf03d9639036e3325b31
Oscar Guerrero <oscar.guerrero@redacreltd.com>
2019-07-20 08:06:42 +0200

    first

---
+++ dist/server.js
@@ -0,0 +1,5 @@
+"use strict";
+const sum = (a, b) => a + b;
+// tslint:disable-next-line:no-console
+setInterval(() => console.log(sum(2, 3)), 1000);
+//# sourceMappingURL=server.js.map
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+const sum = (a: number, b: number) => a + b;
+// tslint:disable-next-line:no-console
+setInterval(() => console.log(sum(2, 3)), 1000);
